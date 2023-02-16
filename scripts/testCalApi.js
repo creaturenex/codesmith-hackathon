a02e207dec03d96ec5dadaa9f2825b5bc6a4ec97
Oscar Romero <creaturenex@gmail.com>
2023-02-16 01:31:45 -0500

    Added todaysDate var

--- scripts/testCalApi.js
+++ scripts/testCalApi.js
@@ -1,5 +1,9 @@
+let todaysDate = new Date()
+
 let url =
   "http://cohort-calendars.us-west-2.elasticbeanstalk.com/calendar/NYOI/03";
+let query = ""
+
 let cache = {};
 async function getData(url) {
   let data = await fetch(url).then((data) => data.json());
